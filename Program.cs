35cef2ec290e0535757d1dcd5325df658c1df404
RenatoAraujo33 <rossorene4@gmail.com>
2023-09-23 15:01:59 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿   <!DOCTYPE html>
+   <html>
+   <head>
+       <title>Swimming Hobby</title>
+   </head>
+   <body>
+       <h1>Bem vindo ao site de natação!</h1>
+       <p>Este é um site para falar sobre o hobby da natação.</p>
+       <ul>
+           <li><a href="/about">Sobre</a></li>
+           <li><a href="/benefits">Benefícios</a></li>
+           <li><a href="/tips">Dicas</a></li>
+       </ul>
+   </body>
+   </html>
+  
